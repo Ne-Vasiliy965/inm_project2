965ef7c41c432d1a42f61f4486847d2272a9a5d7
Ne-Vasiliy965 <abakshin.vs@phystech.edu>
2025-11-07 23:34:32 +0300

    implemented the main things in classes vector, matrix

--- linear_system_solver.cpp
+++ linear_system_solver.cpp
@@ -1,18 +1,117 @@
 #include "linear_system_solver.h"
 
+
+//  --------------Matrix Implementation----------------
 template <class T>
-Matrix::Matrix() noexcept : num_lines_(0), num_columns_(0) {};
+Matrix<T>::Matrix() noexcept : num_lines_(0), num_columns_(0) {};
 
 template <class T>
-Matrix::Matrix(std::vector<std::vector<T>> data) {
+Matrix<T>::Matrix(const std::vector<std::vector<T>>& data) {
   data_ = data;
-  if (!data_.empty()):
-    num_lines_ = data_.size();
+  if (!data_.empty()) {
+    num_rows_ = data_.size();
     num_columns_ = data_[0].size();
   } else {
-    num_lines_ = 0;
+    num_rows_ = 0;
     num_columns_ = 0;
   }
+}
 
-Matrix::~Matrix() noexcept {}
+template <class T>
+Matrix<T>::Matrix(std::vector<std::vector<T>>&& data) {
+  if (!data.empty()) {
+    data_ = std::move(data);
+    num_rows_ = data_.size();
+    num_columns_ = data_[0].size();
 
+  } else {
+    num_rows_ = 0;
+    num_columns_ = 0;
+  }
+}
+
+template <class T>
+Matrix<T>::~Matrix() noexcept {}
+
+template <class T>
+bool Matrix<T>::check_size_is_square() noexcept {
+  return num_columns_ == num_lines_;
+}
+
+template <class T>
+size_t Matrix<T>::get_columns_number() {
+  return num_columns_;
+}
+
+template <class T>
+size_t Matrix<T>::get_rows_number() {
+  return num_rows_;
+}
+
+template <class T>
+std::vector<T>& Matrix<T>::operator[](size_t idx) {
+  return data_[idx]
+}
+
+template <class T>
+const std::vector<T>& Matrix<T>::operator[](size_t idx)  const {
+  return data_[idx]
+}
+
+
+//  --------------Vector Implementation----------------
+
+template <class T>
+size_t Vector<T>::get_size() const {
+  return data_.size();
+}
+
+template <class T>
+T& Vector<T>::operator[](size_t idx) {
+  return data_[idx];
+}
+
+template <class T>
+const T& Vector<T>::operator[](size_t idx) const {
+  return data_[idx];
+}
+
+template <class T>
+Vector<T> operator+(const Vector<T>& lhs, const Vector<T>& rhs) {
+  size_t cur_left_size = lhs.get_size();
+  size_t cur_right_size = rhs.get_size();
+  if (cur_left_size != cur_right_size) {
+    throw std::logic_error("Cannot sum vectors with different sizes");
+  }
+  std::vector tmp(cur_left_size);
+  for (size_t i = 0; i < cur_left_size; ++i) {
+    tmp[i] = lhs[i] + rhs[i];
+  }
+  return Vector(tmp);
+}
+
+template <class T>
+Vector<T> operator-(const Vector<T>& lhs, const Vector<T>& rhs) {
+  size_t cur_left_size = lhs.get_size();
+  size_t cur_right_size = rhs.get_size();
+  if (cur_left_size != cur_right_size) {
+    throw std::logic_error("Cannot substract vectors with different sizes");
+  }
+  std::vector tmp(cur_left_size);
+  for (size_t i = 0; i < cur_left_size; ++i) {
+    tmp[i] = lhs[i] - rhs[i];
+  }
+  return Vector(tmp);
+}
+
+template <class T>
+Vector<T> operator*(const T& multiplier, const Vector<T>& vec) {
+  size_t cur_size = lhs.get_size();
+  std::vector tmp(cur_size);
+  for (size_t i = 0; i < cur_size; ++i) {
+    tmp[i] = multiplier * vec[i];
+  }
+  return Vector(tmp);
+}
+
+//  --------------Solver Implementation----------------
--- linear_system_solver.h
+++ linear_system_solver.h
@@ -3,23 +3,55 @@
 
 #include <iostream>
 #include <vector>
+#include <exception>
 
 template <class T>
-
 class Matrix {
 
  private:
-  size_t num_lines_;
+  size_t num_rows_;
   size_t num_columns_;
   std::vector<std::vector<T>> data_;
 
  public:
 
   Matrix() noexcept;
-  Matrix(std::vector<sdtd::vector<T>> data);
+  Matrix(const std::vector<std::vector<T>>& data);
+  Matrix(std::vector<std::vector<T>>&& data);  //  for r-value
   ~Matrix() noexcept;
+  //  Copy and move constructors to be done ?
+  bool check_size_is_square() noexcept;
+  size_t get_columns_number();
+  size_t get_rows_number();
+
+  //  double check this
+  std::vector<T>& operator[](size_t idx);
+  const std::vector<T>& operator[](size_t idx) const;
+
+  /* can add multiplying matrix by a number and operator + for matrices,
+  but it is not needed in current task */
 };
 
+
+
+template <class T>
+class Vector {
+ private:
+  std::vector<T> data_;
+
+ public:
+  Vector() noexcept default;
+  Vector(const std::vector<T>& data) default;
+  Vector(std::vector<T>&& dat) default;
+  ~Vector() default;
+  size_t get_size() const;
+  T& operator[](size_t idx);
+  const T& operator[](size_t idx) const;
+  //  operator=??
+  friend Vector operator+(const Vector& lhs, const Vector& rhs);
+  friend Vector operator-(const Vector& lhs, const Vector& rhs);
+  friend Vector operator*(const T& multiplier, const Vector& vec);
+};
 
 
 #endif
